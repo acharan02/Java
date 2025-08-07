53e716347562053d2c02574be7431bb8b4a95c3d
acharan02 <charanakkenapally@gmail.com>
2025-08-07 22:58:39 +0530

    Initial commit with java programs

---
+++ conditional statements/if.java
@@ -0,0 +1,11 @@
+public class ifs{
+    public static void main (String[] args){
+    int age = 15;
+    if (age >= 18){
+        System.out.println("Adult");
+    }
+    else{
+        System.out.println("Not Adult");
+    }
+    }
+}
