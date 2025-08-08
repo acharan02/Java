faa4ff437711103772b9c44b59297ff6d1039fc4
charan218a <akkenapallycharan51@gmail.com>
2025-08-08 22:41:55 +0530

    Add new java program

---
+++ conditional statements/num_nature.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+ public class num_nature{
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter the Number: " );
+        int x = input.nextInt();
+        if (x>0){
+            System.out.println(x + " is a Postive number.");
+        }
+        else if (x<0){
+            System.out.println(x + " is a Negative number.");
+        }
+        else{
+            System.out.println("The number is Zero.");
+        }
+        input.close();
+
+    }
+ }
