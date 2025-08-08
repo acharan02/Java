da8f37e3bd95ccf7010c6a98ca53b9858aa6cf01
acharan02 <charanakkenapally@gmail.com>
2025-08-08 22:41:55 +0530

    Add new java program

---
+++ conditional statements/even_odd.java
@@ -0,0 +1,12 @@
+public class even_odd{
+    public static void main(String[] args) {
+        int x = 6;
+        if (x%2==0){
+            System.out.println(x + " is even number.");
+        }
+        else{
+            System.out.println(x + " is an Odd number.");
+        }
+
+    }
+} 
